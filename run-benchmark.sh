0c0c8c15d8c515d2a536362f590d6933c64c2707
Joao Americo <Joao Americo>
2012-07-31 15:01:34 +0200

    Modifying existing files (parent.pom and run-benchmark.sh) to take OSGi/iPOJO benchmark into account

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -18,6 +18,24 @@ cd ../../sca/benchmark-frascati
 mvn -o exec:exec
 cd ../benchmark-tuscany
 mvn -o exec:exec
-cd ../..
 
+# OSGi Benchmark
+cd ../../osgi/benchmark-osgi
+rm -r felix-cache
+mvn -o exec:exec
+
+# iPOJO Benchmark
+cd ../../osgi/benchmark-ipojo
+rm -r felix-cache
+# As Felix does not allow the mvn protocol to retrieve our ipojo-instrumented bundles, I'm doing it manually for the moment
+if test "${M2_REPO+x}" != x; 
+then 
+        echo "$M2_REPO is not set - this benchmark may not work correctly"
+fi
+cp $M2_REPO/org/cbse/container/benchmark/iPOJOFibonacciDelegate/1.0-SNAPSHOT/iPOJOFibonacciDelegate-1.0-SNAPSHOT.jar ../bundles
+cp $M2_REPO/org/cbse/container/benchmark/iPOJOFibonacciIterative/1.0-SNAPSHOT/iPOJOFibonacciIterative-1.0-SNAPSHOT.jar ../bundles
+cp $M2_REPO/org/cbse/container/benchmark/iPOJOFibonacciSimple/1.0-SNAPSHOT/iPOJOFibonacciSimple-1.0-SNAPSHOT.jar ../bundles
+cp $M2_REPO/org/cbse/container/benchmark/iPOJOFibonacciRecursive/1.0-SNAPSHOT/iPOJOFibonacciRecursive-1.0-SNAPSHOT.jar ../bundles
+mvn -o exec:exec
+cd ../..
 echo "Benchmark completed"
